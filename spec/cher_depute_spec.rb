6da3f9abf6e3fa427c72f4566fa182b8dd78cc79
ragenchirm <noel.gagnaire@gmail.com>
2019-04-11 17:50:58 +0200

    test done

--- spec/cher_depute_spec.rb
+++ spec/cher_depute_spec.rb
@@ -4,4 +4,8 @@ describe "The open_page function" do
   it "return an HTML document" do
     expect(open_page("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=600&debut_deputes=500#pagination_deputes").class).to eq(Nokogiri::HTML::Document)
   end
+
+  it "Return the first name and last name of a congressman" do
+    expect(get_congressmen("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=600&debut_deputes=500#pagination_deputes").any?{|a| a[:first_name] == "Marielle" && a[:last_name] == "de Sarnez" && a[:email]=="marielle.desarnez@assemblee-nationale.fr" }).to be true
+  end
 end
