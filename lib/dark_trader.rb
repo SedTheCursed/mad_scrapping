288e531140f526bdb1a9eb57ffe108e59a03c390
ragenchirm <noel.gagnaire@gmail.com>
2019-04-11 13:21:41 +0200

    know we can get an array with the symbols, with we are not using the XPath

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -7,8 +7,11 @@ URL = "https://coinmarketcap.com/all/views/all/"
 
 def crypto_scrapper(url)
   page = Nokogiri::HTML(open(url))
-  page.xpath("//tr")
-  binding.pry
+  raw_symbols = page.css("td.col-symbol")
+  array_of_symbols=[]
+  raw_symbols.each{|symbol|
+  array_of_symbols << symbol.text
+}
 end
 
 crypto_scrapper(URL)
