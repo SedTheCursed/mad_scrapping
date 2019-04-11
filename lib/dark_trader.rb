1ecf89aa267d42c2449ea2dd8ac536dcf0686737
ragenchirm <noel.gagnaire@gmail.com>
2019-04-11 13:27:33 +0200

    We can get an array of the symbols using the xpath line !

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -5,13 +5,22 @@ require 'pry'
 
 URL = "https://coinmarketcap.com/all/views/all/"
 
-def crypto_scrapper(url)
+def crypto_scrapper_with_css(url)
   page = Nokogiri::HTML(open(url))
   raw_symbols = page.css("td.col-symbol")
   array_of_symbols=[]
   raw_symbols.each{|symbol|
-  array_of_symbols << symbol.text
-}
+    array_of_symbols << symbol.text
+  }
 end
 
-crypto_scrapper(URL)
+def crypto_scrapper_with_xpath(url)
+  page = Nokogiri::HTML(open(url))
+  raw_symbols = page.xpath('//td[3]')
+  array_of_symbols=[]
+  raw_symbols.each{|symbol|
+    array_of_symbols << symbol.text
+  }
+end
+
+crypto_scrapper_with_xpath(URL)
