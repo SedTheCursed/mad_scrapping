c1f9dac67dceab5d5db27685f90b84bd99a29ed4
SedTheCursed <sed.robert@gmail.com>
2019-04-11 14:41:51 +0200

    Developpement de Dark_trader

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,12 +1,23 @@
 require_relative "../lib/dark_trader"
 
-describe "The crypto_scrapper function" do
+describe "The crypto_scrapper_with_css function" do
   it "returns an array not null" do
-    expect(crypto_scrapper("https://coinmarketcap.com/all/views/all/")).not_to eq(nil)
+    expect(crypto_scrapper_with_css("https://coinmarketcap.com/all/views/all/")).not_to eq(nil)
   end
   
   it "returns an array bigger than a thousand" do
-    cryptos = crypto_scrapper("https://coinmarketcap.com/all/views/all/")
+    cryptos = crypto_scrapper_with_css("https://coinmarketcap.com/all/views/all/")
+    expect(cryptos.count).to be > 1000
+  end
+end
+
+describe "The crypto_scrapper_with_xpath function" do
+  it "returns an array not null" do
+    expect(crypto_scrapper_with_xpath("https://coinmarketcap.com/all/views/all/")).not_to eq(nil)
+  end
+  
+  it "returns an array bigger than a thousand" do
+    cryptos = crypto_scrapper_with_xpath("https://coinmarketcap.com/all/views/all/")
     expect(cryptos.count).to be > 1000
   end
 end
